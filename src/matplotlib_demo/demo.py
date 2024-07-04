4b1e3548d576579d7137941bc5a6e6504a488da2
Bojan Komazec <1876278+BojanKomazec@users.noreply.github.com>
2024-07-04 23:56:44 +0100

    Added matplotlib demo which draws directed graph

--- src/matplotlib_demo/demo.py
+++ src/matplotlib_demo/demo.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import networkx as nx
 
 def plot_demo():
 	x = np.linspace(-10, 10, 20)
@@ -15,5 +16,41 @@ def plot_demo():
 	plt.plot(x, y, linestyle='--', marker='o', color='b')
 	plt.show()
 
+def directed_graph_demo():
+
+	# Create a directed graph
+	G = nx.DiGraph()
+
+	# Add nodes and edges
+	components = [
+		("VMware vCenter Server", "VMware ESXi"),
+		("VMware vSphere Client", "VMware vCenter Server"),
+		("Virtual Machines (VMs)", "VMware ESXi"),
+		("VMware vSAN", "VMware ESXi"),
+		("VMware NSX", "VMware ESXi"),
+		("VMware vSphere Client", "VMware ESXi"),
+		("VMware Site Recovery Manager", "VMware vCenter Server"),
+		("VMware High Availability (HA)", "VMware ESXi"),
+		("VMware Fault Tolerance (FT)", "VMware ESXi"),
+		("VMware vSphere Distributed Resource Scheduler (DRS)", "VMware vCenter Server"),
+	]
+
+	# Adding nodes and edges to the graph
+	G.add_edges_from(components)
+
+	# Draw the graph
+	pos = nx.spring_layout(G, seed=42)
+	plt.figure(figsize=(12, 8))
+
+	# Draw nodes and edges
+	nx.draw(G, pos, with_labels=True, node_size=3000, node_color="skyblue", font_size=10, font_weight="bold", arrowsize=20)
+
+	# Display the diagram
+	plt.title("Relations between VMware ESXi and Other VMware Components")
+	plt.show()
+
+
+
 def matplotlib_demo():
-	plot_demo()
+	# plot_demo()
+	directed_graph_demo()
